111ff083a3914adc37f5bf929ccb5c98b773b9e2
Tarnovskii <artem3dgame@gmail.com>
2020-10-06 07:48:32 +0300

    complete main page

---
+++ public/src/components/LinkTile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import s from '../stylesheets/link.module.css'
+
+export default () => {
+    return (
+        <div className={s.wrapper}>
+            <div className={s.preview}>
+                PREVIEW
+            </div>
+            <p className={s.header}>Описанние данной сылки на данный ресурс</p>
+            <p className={s.description}> Описание тут получится немного длиннее чем обычно, просто потому что мне
+                хочется, не более того. </p>
+        </div>
+    )
+}
