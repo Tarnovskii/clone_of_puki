0b0f139c8dfa41b3b0efcc29e987f23632e809bf
Tarnovskii <artem3dgame@gmail.com>
2020-10-06 03:52:51 +0300

    header complete

--- public/src/components/Header.jsx
+++ public/src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import s from '../stylesheets/header.module.css'
+
+import logo from '../img/logo.svg'
 
 export default () => {
     return (
-        <header>
-
+        <header className={s.header_wrapper}>
+            <img className={s.logo_wrapper} src={logo} alt={'logo'}/>
+            <div className={s.header_title}>
+                <h1>ПУКІ</h1>
+                <h2>Посібник к комп'ютерній інженерії</h2>
+            </div>
+            <a className={s.lb} href={'/#'}>Войти в систему</a>
         </header>
     )
 }
