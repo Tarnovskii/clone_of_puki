0b6ce2ba525f4af8f5685c2f84d2ed0a6617aade
Tarnovskii <artem3dgame@gmail.com>
2020-10-15 04:00:28 +0300

    PUKI-29 Some fixes and addiotions with calendar

--- public/src/components/routes/StudentProfile.jsx
+++ public/src/components/routes/StudentProfile.jsx
@@ -7,6 +7,7 @@ import FileBrowser from "../../components/global/FileBrowser";
 import UsefullLinks from "../global/UsefullLinks";
 import {mapDispatchToProps} from "../../utils/storeUtils/dispatchToProps";
 import {connect} from "react-redux";
+import Calendar from "../../containers/global/Calendar";
 
 export default connect(null, mapDispatchToProps())((props) => {
     props.routingActions.updateFooterStatus('visible')
@@ -18,13 +19,7 @@ export default connect(null, mapDispatchToProps())((props) => {
             </section>
             <section className={s.content}>
                 <h3 className={s.bh3}>Добро пожаловать, <span>Jennifer Ktototam</span></h3>
-                <div className={s.calendar}>
-                    <h3 className={s.bh3}>Календарь событий</h3>
-                    <div className={s.controller}>
-                        <img src={arrow}/> Октябрь 2020 <img src={arrow}/>
-                    </div>
-                    Тут будет календарь
-                </div>
+                <Calendar/>
                 <div className={s.progress}>
                     <h3 className={s.bh3}>Успеваемость устудента</h3>
                     <Perfomance subj={'Програмирование II'}/>
